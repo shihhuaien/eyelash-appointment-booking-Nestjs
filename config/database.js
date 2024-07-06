cd4d9745c36f2b76a472b4237ac342aecbd4d928
shihhuaien <timothyshih12032007@gmail.com>
2024-07-06 14:56:48 +0800

    feat(database): configure Supabase PostgreSQL database
    Configured the application to use Supabase PostgreSQL database.
    - Updated database configuration in config/database.js

--- config/database.js
+++ config/database.js
@@ -55,7 +55,7 @@ module.exports = ({ env }) => {
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
+        ssl: env.bool('DATABASE_SSL', true) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
           ca: env('DATABASE_SSL_CA', undefined),
@@ -63,7 +63,7 @@ module.exports = ({ env }) => {
           cipher: env('DATABASE_SSL_CIPHER', undefined),
           rejectUnauthorized: env.bool(
             'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true
+            false
           ),
         },
         schema: env('DATABASE_SCHEMA', 'public'),
